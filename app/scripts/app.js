f5c3cfeebc3e4d7aca517fb0257d419c4229ce31
Roberto André Bocanegra Palacios <rabp_91@hotmail.com>
2017-08-10 00:43:44 -0500

    First

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name transnvFrontendApp
+ * @description
+ * # transnvFrontendApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('transnvFrontendApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name transnvFrontendApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the transnvFrontendApp
+ */
+angular.module('transnvFrontendApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
