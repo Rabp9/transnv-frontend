9dfdceb1921b74cdef7edc5b414513e0c7818472
tmtrbocanegra <rabp_91@hotmail.com>
2017-11-20 17:21:15 -0500

    Avanzando

--- app/scripts/controllers/noticias.js
+++ app/scripts/controllers/noticias.js
@@ -8,10 +8,22 @@
  * Controller of the transnvFrontendApp
  */
 angular.module('transnvFrontendApp')
-  .controller('NoticiasCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+.controller('NoticiasCtrl', function ($scope, noticiasservice, ngProgressFactory, 
+    cabecerasservice, $state, $q, $rootScope) {
+        
+    $scope.init = function() {
+        $scope.progressbar = ngProgressFactory.createInstance();
+        $scope.progressbar.start();
+        return $q.all([
+            noticiasservice.get({id: $state.params.id}).$promise,
+            cabecerasservice.getByDescripcion({descripcion: 'noticia-cabecera'}).$promise
+        ]).then(function(data) {
+            $scope.noticia = data[0].noticia;
+            $scope.imagen = 'img/cabeceras/' + data[1].cabecera.imagen;
+            $rootScope.title = $scope.noticia.titulo; 
+            $scope.progressbar.complete();
+        });
+    };
+    
+    $scope.init();
+});
