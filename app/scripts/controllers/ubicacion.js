6b57602b76b3f4301e8a45dacc7c933e463aba32
tmtrbocanegra <rabp_91@hotmail.com>
2017-12-06 09:29:23 -0500

    Avanzando

--- app/scripts/controllers/ubicacion.js
+++ app/scripts/controllers/ubicacion.js
@@ -8,10 +8,22 @@
  * Controller of the transnvFrontendApp
  */
 angular.module('transnvFrontendApp')
-  .controller('UbicacionCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+.controller('UbicacionCtrl', function ($scope, ngProgressFactory, infosservice,
+    cabecerasservice, $q) {
+    var search = ['ubicacion_code'];
+    
+    $scope.init = function() {
+        $scope.progressbar = ngProgressFactory.createInstance();
+        $scope.progressbar.start();
+        return $q.all([
+            infosservice.getDataMany(search).$promise,
+            cabecerasservice.getByDescripcion({descripcion: 'ubicacion-cabecera'}).$promise
+        ]).then(function(data) {
+            $scope.infos = data[0].info;
+            $scope.imagen = 'img/cabeceras/' + data[1].cabecera.imagen;
+            $scope.progressbar.complete();
+        });
+    };
+    
+    $scope.init();
+});
