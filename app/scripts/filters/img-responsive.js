106d31dbc7065dc8ee8028b6e026a6a589b84dd7
tmtrbocanegra <rabp_91@hotmail.com>
2018-01-22 10:49:45 -0500

    Avanzando

--- app/scripts/filters/img-responsive.js
+++ app/scripts/filters/img-responsive.js
@@ -12,7 +12,14 @@ angular.module('transnvFrontendApp')
 .filter('imgResponsive', function () {
     return function (input, size) {
         var ext = input.split('.').pop();
-        input = input.replace(/\..+$/, '');
+        
+        for (var i = input.length - 1; i > 0; i--) {
+            if (input.charAt(i) === '.') {
+                input = input.slice(0, i);
+                break;
+            }
+        }
+        
         input = input + size + '.' + ext;
         return input;
     };
