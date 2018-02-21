8d1fb05c9cbee376ca637b010dd863c136f829f8
tmtrbocanegra <rabp_91@hotmail.com>
2018-02-21 14:17:33 -0500

    Avanzando

--- app/scripts/controllers/servicios.js
+++ app/scripts/controllers/servicios.js
@@ -9,18 +9,18 @@
  */
 angular.module('transnvFrontendApp')
 .controller('ServiciosCtrl', function ($scope, serviciosservice, ngProgressFactory, 
-    cabecerasservice, $state, $q, $rootScope) {
+    $state, $q, $rootScope) {
     $scope.init = function() {
         $scope.progressbar = ngProgressFactory.createInstance();
         $scope.progressbar.start();
-        return $q.all([
-            serviciosservice.get({id: $state.params.id}).$promise,
-            cabecerasservice.getByDescripcion({descripcion: 'servicio-cabecera'}).$promise
-        ]).then(function(data) {
-            $scope.servicio = data[0].servicio;
-            $scope.imagen = 'img/cabeceras/' + data[1].cabecera.imagen;
+        serviciosservice.get({id: $state.params.id}, function(data) {
+            $scope.servicio = data.servicio;
+            var imagen = 'img/servicios/' + $scope.servicio.portada;
             $rootScope.title = $scope.servicio.titulo; 
             $scope.progressbar.complete();
+            $scope.style = {
+                backgroundImage: 'url("' + $rootScope.path_location + imagen + '")'
+            };
         });
     };
     
